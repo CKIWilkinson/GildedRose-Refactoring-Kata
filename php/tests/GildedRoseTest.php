b223b31b47471cabcacc458c4088404f628125e3
Christian <ckiwilkinson@gmail.com>
2023-09-24 21:06:03 +0100

    Add new unit tests

--- php/tests/GildedRoseTest.php
+++ php/tests/GildedRoseTest.php
@@ -10,11 +10,126 @@ use PHPUnit\Framework\TestCase;
 
 class GildedRoseTest extends TestCase
 {
+    const SULFURAS = 'Sulfuras, Hand of Ragnaros';
+    const AGED_BRIE = 'Aged Brie';
+    const CONJURED_MANA_CAKE = 'Conjured Mana Cake';
+    const BACKSTAGE_PASS = 'Backstage passes to a TAFKAL80ETC concert';
+
     public function testFoo(): void
     {
         $items = [new Item('foo', 0, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
         $this->assertSame('foo', $items[0]->name);
+    }
+
+    public function testQualityReduces(): void
+    {
+        $items = [new Item('item', 2, 2)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(1, $item->sellIn);
+        $this->assertSame(1, $item->quality);
+    }
+
+    public function testMultipleItems(): void
+    {
+        $items = [
+            new Item('item', 2, 2),
+            new Item('other item', 4, 5),
+        ];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $this->assertSame('item', $items[0]->name);
+        $this->assertSame(1, $items[0]->sellIn);
+        $this->assertSame(1, $items[0]->quality);
+
+        $this->assertSame('other item', $items[1]->name);
+        $this->assertSame(3, $items[1]->sellIn);
+        $this->assertSame(4, $items[1]->quality);
+    }
+
+    public function testQualityImproves(): void
+    {
+        $items = [new Item(self::AGED_BRIE, 2, 2)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(3, $item->quality);
+    }
+
+    public function testLegendaryItem(): void
+    {
+        $items = [new Item(self::SULFURAS, 2, 2)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(2, $item->quality);
+    }
+
+    public function testQualityDoesNotExceed50(): void
+    {
+        $items = [new Item(self::AGED_BRIE, 2, 50)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(50, $item->quality);
+    }
+
+    public function testBackstageSellInAbove10(): void
+    {
+        $items = [new Item(self::BACKSTAGE_PASS, 11, 25)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(26, $item->quality);
+    }
+
+    public function testBackstageSellInBelow10(): void
+    {
+        $items = [new Item(self::BACKSTAGE_PASS, 10, 25)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(27, $item->quality);
+    }
+
+    public function testBackstageSellInBelow5(): void
+    {
+        $items = [new Item(self::BACKSTAGE_PASS, 5, 25)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(28, $item->quality);
+    }
+
+    public function testBackstageSellInBelow1(): void
+    {
+        $items = [new Item(self::BACKSTAGE_PASS, 0, 25)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(0, $item->quality);
+    }
+
+    public function testConjuredItemDegradation(): void
+    {
+        $items = [new Item(self::CONJURED_MANA_CAKE, 2, 50)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+
+        $item = $items[0];
+        $this->assertSame(1, $item->sellIn);
+        $this->assertSame(48, $item->quality);
     }
 }
